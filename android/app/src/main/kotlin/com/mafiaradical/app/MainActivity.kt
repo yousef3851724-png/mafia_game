5751a99da0f13c88278907e5c3189b2beaa48d6c
Yousef <299321388+yousef3851724-png@users.noreply.github.com>
2026-08-11 13:59:37 +0330

    Update MainActivity.kt

--- android/app/src/main/kotlin/com/mafiaradical/app/MainActivity.kt
+++ android/app/src/main/kotlin/com/mafiaradical/app/MainActivity.kt
@@ -3,3 +3,8 @@ package com.mafiaradical.app
 import io.flutter.embedding.android.FlutterActivity
 
 class MainActivity : FlutterActivity()
+package com.radical.mafia
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
