8f550fd4414cc48fcdec0ff98a2a708f7e0b1b6c
Yousef <299321388+yousef3851724-png@users.noreply.github.com>
2026-08-13 01:23:21 +0330

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,39 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
-import 'logic/game_controller.dart';
-import 'screens/home_screen.dart';
-
+import 'app.dart';
 void main() {
-  runApp(
-    ChangeNotifierProvider(
-      create: (_) => GameController(),
-      child: const MafiaApp(),
-    ),
-  );
-}
-
-class MafiaApp extends StatelessWidget {
-  const MafiaApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'مافیا',
-      theme: ThemeData.dark().copyWith(
-        primaryColor: Colors.red[900],
-        scaffoldBackgroundColor: Colors.grey[900],
-        appBarTheme: const AppBarTheme(
-          backgroundColor: Colors.red,
-          foregroundColor: Colors.white,
-        ),
-        dialogTheme: DialogTheme(
-          backgroundColor: Colors.grey[800],
-          titleTextStyle: const TextStyle(color: Colors.white, fontSize: 22),
-          contentTextStyle: const TextStyle(color: Colors.white70),
-        ),
-      ),
-      home: const HomeScreen(),
-      debugShowCheckedModeBanner: false,
-    );
-  }
+  runApp(const MafiaRadicalApp());
 }
