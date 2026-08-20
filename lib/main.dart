2f334b4958e2794893472dff3c3eaceda0947f55
Yousef <299321388+yousef3851724-png@users.noreply.github.com>
2026-08-21 02:28:29 +0330

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,33 @@
 import 'package:flutter/material.dart';
-import 'app.dart';
 
 void main() {
-  runApp(const MafiaRadicalApp());
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Mafia Radical',
+      theme: ThemeData.dark(),
+      home: const HomeScreen(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Mafia Radical')),
+      body: const Center(
+        child: Text('Welcome to Mafia Radical!'),
+      ),
+    );
+  }
 }
