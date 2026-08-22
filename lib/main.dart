56d35eb3c3ae1c0234b57211c866a7f4c97f85f4
Yousef <299321388+yousef3851724-png@users.noreply.github.com>
2026-08-22 15:45:17 +0330

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,28 @@
 import 'package:flutter/material.dart';
-import 'app.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:mafia_game/bloc/game_bloc.dart';
+import 'package:mafia_game/router/app_router.dart';
 
 void main() {
-  runApp(const MafiaRadicalApp());
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocProvider(
+      create: (context) => GameBloc(),
+      child: MaterialApp.router(
+        title: 'مافیا رادیکال',
+        theme: ThemeData(
+          primarySwatch: Colors.deepPurple,
+          useMaterial3: true,
+        ),
+        routerConfig: appRouter,
+        debugShowCheckedModeBanner: false,
+      ),
+    );
+  }
 }
