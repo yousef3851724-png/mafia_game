cacae9338d416e95ca0b0d113b4d2d8cf767cc80
Yousef <299321388+yousef3851724-png@users.noreply.github.com>
2026-08-22 16:36:17 +0330

    Update app_router.dart

--- lib/router/app_router.dart
+++ lib/router/app_router.dart
@@ -1,9 +1,17 @@
 import 'package:go_router/go_router.dart';
 import 'package:mafia_game/screens/home_screen.dart';
 import 'package:mafia_game/screens/game_screen.dart';
+import 'package:mafia_game/screens/splash_screen.dart';
+import 'package:mafia_game/screens/settings_screen.dart';
+import 'package:mafia_game/screens/history_screen.dart';
 
 final appRouter = GoRouter(
+  initialLocation: '/splash',
   routes: [
+    GoRoute(
+      path: '/splash',
+      builder: (context, state) => const SplashScreen(),
+    ),
     GoRoute(
       path: '/',
       builder: (context, state) => const HomeScreen(),
@@ -14,6 +22,14 @@ final appRouter = GoRouter(
         final playerNames = state.extra as List<String>? ?? [];
         return GameScreen(playerNames: playerNames);
       },
+    ),
+    GoRoute(
+      path: '/settings',
+      builder: (context, state) => const SettingsScreen(),
+    ),
+    GoRoute(
+      path: '/history',
+      builder: (context, state) => const HistoryScreen(),
     ),
   ],
 );
