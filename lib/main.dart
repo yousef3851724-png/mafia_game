6892a8cfcd0948809f6a8c46750c54fc7c409f2e
Amirali <you@example.com>
2026-08-25 00:32:51 +0330

    add main.dart and svg assets

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'app.dart';
 
 void main() {
-  runApp(const MafiaRadicalApp());
-}
+  runApp(
+    const ProviderScope(
+      child: MafiaRadicalApp(),
+    ),
