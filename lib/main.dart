145d27db4d025f93845fb8e8a40401c4f2925d2e
Yousef <299321388+yousef3851724-png@users.noreply.github.com>
2026-08-26 04:33:55 +0330

    Change app widget from MafiaApp to MafiaRadicalApp
    Updated the main app widget from MafiaApp to MafiaRadicalApp.

--- lib/main.dart
+++ lib/main.dart
@@ -3,5 +3,9 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'app.dart';
 
 void main() {
-  runApp(const ProviderScope(child: MafiaApp()));
+  runApp(
+    const ProviderScope(
+      child: MafiaRadicalApp(),  // ← قبلاً MafiaApp بود
+    ),
+  );
 }
