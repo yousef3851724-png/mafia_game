70a97f9f0ab1fca5f0fe20ae2e360aebd1e8d403
Yousef <299321388+yousef3851724-png@users.noreply.github.com>
2026-08-28 15:32:44 +0330

    Update app_logger.dart

--- lib/logger/app_logger.dart
+++ lib/logger/app_logger.dart
@@ -184,3 +184,22 @@ class AIFactory {
     );
   }
 }
+import 'package:logger/logger.dart';
+
+class AppLogger {
+  static final Logger _logger = Logger(
+    printer: PrettyPrinter(
+      methodCount: 2,
+      errorMethodCount: 8,
+      lineLength: 120,
+      colors: true,
+      printEmojis: true,
+    ),
+  );
+
+  static void d(dynamic message) => _logger.d(message);
+  static void i(dynamic message) => _logger.i(message);
+  static void w(dynamic message) => _logger.w(message);
+  static void e(dynamic message, [dynamic error, StackTrace? stackTrace]) =>
+      _logger.e(message, error: error, stackTrace: stackTrace);
+}
