b9614aa615981c83063b1e9f1943c20e8b21cf8c
Yousef <299321388+yousef3851724-png@users.noreply.github.com>
2026-08-29 02:31:07 +0330

    Update player.dart

--- lib/features/game/domain/entities/player.dart
+++ lib/features/game/domain/entities/player.dart
@@ -1,31 +1,122 @@
-class Player {
+"lib/features/game/domain/entities/player.dart"
+
+import 'package:equatable/equatable.dart';
+
+class Player extends Equatable {
   final String id;
   final String name;
-  final String role;
-  final bool isAlive;
-  final int votesReceived;
+  final bool isHost;
+  final String? vote;
 
   const Player({
     required this.id,
     required this.name,
-    required this.role,
-    this.isAlive = true,
-    this.votesReceived = 0,
+    this.isHost = false,
+    this.vote,
   });
 
   Player copyWith({
     String? id,
     String? name,
-    String? role,
-    bool? isAlive,
-    int? votesReceived,
+    bool? isHost,
+    String? vote,
+    bool clearVote = false,
   }) {
     return Player(
       id: id ?? this.id,
       name: name ?? this.name,
-      role: role ?? this.role,
-      isAlive: isAlive ?? this.isAlive,
-      votesReceived: votesReceived ?? this.votesReceived,
+      isHost: isHost ?? this.isHost,
+      vote: clearVote ? null : (vote ?? this.vote),
+    );
+  }
+
+  @override
+  List<Object?> get props => [id, name, isHost, vote];
+}
+
+"lib/features/game/domain/repositories/i_game_repository.dart"
+
+import '../entities/player.dart';
+
+abstract class IGameRepository {
+  Stream<List<Player>> watchPlayers();
+
+  Stream<Map<String, int>> watchVotes();
+
+  Future<String> joinRoom(
+    String roomId,
+    String playerName,
+  );
+
+  Future<void> submitVote(
+    String roomId,
+    String playerId,
+    String option,
+  );
+
+  Future<void> leaveRoom(
+    String roomId,
+    String playerId,
+  );
+
+  Future<void> dispose();
+}
+
+"lib/features/game/domain/usecases/join_room_usecase.dart"
+
+import '../repositories/i_game_repository.dart';
+
+class JoinRoomUseCase {
+  final IGameRepository repository;
+
+  const JoinRoomUseCase(this.repository);
+
+  Future<String> call(
+    String roomId,
+    String playerName,
+  ) {
+    return repository.joinRoom(roomId, playerName);
+  }
+}
+
+"lib/features/game/domain/usecases/submit_vote_usecase.dart"
+
+import '../repositories/i_game_repository.dart';
+
+class SubmitVoteUseCase {
+  final IGameRepository repository;
+
+  const SubmitVoteUseCase(this.repository);
+
+  Future<void> call(
+    String roomId,
+    String playerId,
+    String option,
+  ) {
+    return repository.submitVote(
+      roomId,
+      playerId,
+      option,
+    );
+  }
+}
+
+"lib/features/game/domain/usecases/leave_room_usecase.dart"
+
+import '../repositories/i_game_repository.dart';
+
+class LeaveRoomUseCase {
+  final IGameRepository repository;
+
+  const LeaveRoomUseCase(this.repository);
+
+  Future<void> call(
+    String roomId,
+    String playerId,
+  ) {
+    return repository.leaveRoom(
+      roomId,
+      playerId,
     );
   }
 }
