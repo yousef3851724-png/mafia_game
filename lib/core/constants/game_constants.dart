3e3d35f21bd4213f4dfdc447b1f425d740bd3b3f
Yousef <299321388+yousef3851724-png@users.noreply.github.com>
2026-08-28 15:25:50 +0330

    Update game_constants.dart

--- lib/core/constants/game_constants.dart
+++ lib/core/constants/game_constants.dart
@@ -1,22 +1,9 @@
-enum GameRole { mafia, citizen, doctor, detective }
-
-class GameConstants {
-  static const int minPlayers = 6;
-  static const int maxPlayers = 30;
-  static const int mafiaCount = 2;
-  static const Duration nightDuration = Duration(seconds: 45);
-  static const Duration dayDuration = Duration(minutes: 2);
-
-  static String getRoleName(GameRole role) {
-    switch (role) {
-      case GameRole.mafia:
-        return 'مافیا';
-      case GameRole.citizen:
-        return 'شهروند';
-      case GameRole.doctor:
-        return 'پزشک';
-      case GameRole.detective:
-        return 'کارآگاه';
-    }
-  }
+class AppConstants {
+  static const String appName = 'دوز';
+  static const int boardSize = 3;
+  static const String emptyCell = '';
+  static const String playerX = 'X';
+  static const String playerO = 'O';
+  static const String settingsKeyTheme = 'theme_mode';
+  static const String settingsKeySound = 'sound_enabled';
 }
