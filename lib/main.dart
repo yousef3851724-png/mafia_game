2009a6778043f1b913bc9cbb1f0afae4e25be64a
Yousef <299321388+yousef3851724-png@users.noreply.github.com>
2026-08-28 15:23:40 +0330

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'app.dart';
+import 'dependency_injection/injection_container.dart';
 
-void main() {
-  runApp(const MafiaRadicalApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await initDependencies();
+  runApp(const ProviderScope(child: MyApp()));
 }
