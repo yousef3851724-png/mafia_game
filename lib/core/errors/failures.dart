1b223bce92825c2220b8e79c1a2efd549e7ca4e1
Yousef <299321388+yousef3851724-png@users.noreply.github.com>
2026-08-28 15:27:25 +0330

    Update failures.dart

--- lib/core/errors/failures.dart
+++ lib/core/errors/failures.dart
@@ -29,3 +29,19 @@ class NetworkFailure extends Failure {
 class UnknownFailure extends Failure {
   const UnknownFailure([super.message = 'خطای ناشناخته رخ داد']);
 }
+abstract class Failure {
+  final String message;
+  const Failure(this.message);
+}
+
+class DatabaseFailure extends Failure {
+  const DatabaseFailure(super.message);
+}
+
+class StorageFailure extends Failure {
+  const StorageFailure(super.message);
+}
+
+class ValidationFailure extends Failure {
+  const ValidationFailure(super.message);
+}
