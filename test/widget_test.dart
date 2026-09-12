74cb3dc1cc5800997a9dd532d95ceeef5e88cbc7
yousef h <299321388+yousef3851724-png@users.noreply.github.com>
2026-09-13 02:06:37 +0330

    Update widget_test.dart

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,11 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 import 'package:mafia_radical/main.dart';
 
 void main() {
   testWidgets('App launches and shows the scenario lobby screen',
       (WidgetTester tester) async {
+    TestWidgetsFlutterBinding.ensureInitialized();
+    SharedPreferences.setMockInitialValues({});
+
     await tester.pumpWidget(const MafiaGame());
     await tester.pumpAndSettle();
 
