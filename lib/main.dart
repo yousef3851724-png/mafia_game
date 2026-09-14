bc808579555ccf71a0e8c3d0ee20e7210505f47e
yousef h <299321388+yousef3851724-png@users.noreply.github.com>
2026-09-14 06:11:28 +0330

    ui: enforce Radical dark theme at app root

--- lib/main.dart
+++ lib/main.dart
@@ -14,14 +14,13 @@ class MafiaRadicalApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final radical = RadicalTheme.dark();
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'مافیا رادیکال',
-      theme: radical,
-      darkTheme: radical,
+      theme: RadicalTheme.dark(),
+      darkTheme: RadicalTheme.dark(),
       themeMode: ThemeMode.dark,
       home: const CinematicHomeScreen(),
+      debugShowCheckedModeBanner: false,
+      title: 'مافیا رادیکال',
     );
   }
 }
