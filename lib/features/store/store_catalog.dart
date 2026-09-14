84245eedbcac7372f41745e075ba5871d273c32d
yousef h <299321388+yousef3851724-png@users.noreply.github.com>
2026-09-14 04:35:43 +0330

    feat: seed avatar store catalog with five purchasable items

---
+++ lib/features/store/store_catalog.dart
@@ -0,0 +1,10 @@
+import '../../core/models/app_models.dart';
+
+/// Data-only catalog. UI is intentionally untouched.
+const avatarStoreItems = <AvatarItem>[
+  AvatarItem(id: 'avatar_shadow', name: 'سایه شب', priceDiamonds: 100, assetPath: 'assets/images/avatar_shadow.svg'),
+  AvatarItem(id: 'avatar_detective', name: 'بازرس', priceDiamonds: 200, assetPath: 'assets/images/avatar_detective.svg'),
+  AvatarItem(id: 'avatar_crimson', name: 'قرمز مرموز', priceDiamonds: 500, assetPath: 'assets/images/avatar_crimson.svg'),
+  AvatarItem(id: 'avatar_gold', name: 'طلایی رادیکال', priceDiamonds: 200, assetPath: 'assets/images/avatar_gold.svg'),
+  AvatarItem(id: 'avatar_noir', name: 'نوآر', priceDiamonds: 500, assetPath: 'assets/images/avatar_noir.svg'),
+];
