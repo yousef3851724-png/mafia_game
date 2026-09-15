c3b96f9f064bac1c6ddfdb7cd5a1f782b89b4166
yousef h <299321388+yousef3851724-png@users.noreply.github.com>
2026-09-16 02:44:03 +0330

    Connect ProviderScope and central go_router app shell

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import 'core/theme/radical_theme.dart';
-import 'features/home/cinematic_home_screen.dart';
+import 'router/app_router.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
@@ -14,13 +15,20 @@ class MafiaRadicalApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return MaterialApp.router(
       theme: RadicalTheme.dark(),
       darkTheme: RadicalTheme.dark(),
       themeMode: ThemeMode.dark,
-      home: const CinematicHomeScreen(),
+      routerConfig: radicalRouter,
       debugShowCheckedModeBanner: false,
       title: 'مافیا رادیکال',
+      locale: const Locale('fa', 'IR'),
+      supportedLocales: const [Locale('fa', 'IR'), Locale('en', 'US')],
+      localizationsDelegates: const [
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+      ],
     );
   }
 }
